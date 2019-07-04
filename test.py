ccf26bbf2dad8c5efb6cdd929e20fbd4f3701076
kharaa59 <anthony.dommery@gmail.com>
2019-07-04 09:13:59 +0200

    Ajout le téléchargement des éléments PHP7.2 nécessaires

--- test.py
+++ test.py
@@ -71,7 +71,9 @@ class ApacheElem:
 
 
 class PhpElem:
-    pass
+    def installPhp(self):
+        """Installation du service PHP 7.2"""
+        apt_get_install(['php7.2', 'libapache2-mod-php7.2', 'php7.2-common', 'php7.2-mbstring', 'php7.2-xmlrpc', 'php7.2-soap', 'php7.2-gd', 'php7.2-xml', 'php7.2-intl', 'php7.2-mysql', 'php7.2-cli', 'php7.2-zip', 'php7.2-curl'])
 
 class MariaDbElem:
     def __init__(self, password):
