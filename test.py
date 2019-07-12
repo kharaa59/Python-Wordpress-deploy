552f5bf572478862a2200fc1d472e42ef0347560
kharaa59 <anthony.dommery@gmail.com>
2019-07-12 14:34:22 +0200

    Ajout des fichiers de conf

--- test.py
+++ test.py
@@ -84,10 +84,20 @@ class PhpElem:
     def installPhp(self):
         """Installation du service PHP 7.2"""
         apt_get_install(['php7.2', 'libapache2-mod-php7.2', 'php7.2-common', 'php7.2-mbstring', 'php7.2-xmlrpc', 'php7.2-soap', 'php7.2-gd', 'php7.2-xml', 'php7.2-intl', 'php7.2-mysql', 'php7.2-cli', 'php7.2-zip', 'php7.2-curl'])
+        
+    def configurationPhp(self):
+        phpIniTemplate= open("configuration_files/php.ini","r")
+        phpIni= open("/etc/php/7.2/apache2/php.ini","w")
+        phpIni.write(phpIniTemplate.read())
+        phpIni.close()
+        phpIniTemplate.close()
 
 class MariaDbElem:
-    def __init__(self, password):
+    def __init__(self, password, wpdb, wpuser, wppassword):
         self.password = password
+        self.wpdb = wpdb
+        self.wpuser = wpuser
+        self.wppassword = wppassword
     def installMariaDb(self):
         """Installation du service via l'apt-get"""
         apt_get_install(['mariadb-server', 'mariadb-client'])
@@ -104,7 +114,8 @@ class MariaDbElem:
         DELETE FROM mysql.user WHERE User=""; \
         DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); \
         DROP DATABASE IF EXISTS test; \
-        FLUSH PRIVILEGES;"
+        FLUSH PRIVILEGES; \
+        EXIT"
 
         try:
             conn = MySQLdb.connect(**paramMysql)
@@ -113,10 +124,54 @@ class MariaDbElem:
         except MySQLdb.Error, e:
             print "Error %d: %s" % (e.args[0],e.args[1])
             sys.exit(1)
-
-        
     
+    def createWpDataBase(self):
+        paramMysql = {
+            "host" = "localhost",
+            "user" = "root",
+            "passwd" = self.password,
+            "db" = "myBase",
+        }
+        query = "CREATE DATABASE "+self.wpdb+"; \
+        CREATE USER '"+self.wpuser+"'@'localhost' IDENTIFIED BY"+self.wppassword+"; \
+        GRANT ALL ON "+self.wpdb+".* TO '"+self.wpuser+"'@'localhost' IDENTIFIED BY '"+self.wppassword+"' WITH GRANT OPTION; \
+        FLUSH PRIVILEGES; \
+        EXIT"
+        
+        try:
+            conn = MySQLdb.connect(**paramMysql)
+            cur = conn.cursor(MySQLdb.cursors.DictCursor)
+            cur.execute(query)
+        except MySQLdb.Error, e:
+            print "Error %d: %s" % (e.args[0],e.args[1])
+            sys.exit(1)
 
 class WordpressElem:
-    pass
+    """Installation de Wordpress dans le dossier défini dans le dossier de configuration"""
+    def downloadWp(self):
+        try:
+            subprocess.call(['mkdir /var/www/html/example.com'])
+        except OSError:
+            print("Un erreur s'est produite lors de la création du dossier Wordpress")
+        try:
+            subprocess.call(['cd /tmp'])
+        except OSError:
+            print("Une erreur s'est produite lors de l'acces au dossier /tmp")
+        try:
+            subprocess.call(['wget https://wordpress.org/latest.tar.gz'])
+        except OSError:
+            print("Une erreur s'est produite lors du téléchargement de Wordpress")
+        try:
+            subprocess.call(['tar -xvzf latest.tar.gz'])
+        except OSError:
+            print("Une erreur s'est produite lors de l'extraction de Wordpress")
+        try:
+            subprocess.call(['mv wordpress /var/www/html/example.com'])
+        except OSError:
+            print("Une erreur s'est produite lors du déplacement du dossier Wordpress au répertoire défini")
+        try:
+            subprocess.call(['chown -R www-data:www-data /var/www/html/example.com'])
+            subprocess.call(['chown -R 755 /var/www/html/example.com'])
+        except OSError:
+            print("Une erreur s'est produite lors de la modification des droits du dossier")
 
